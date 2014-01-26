3fc80753a3b25dd9a24b159d35ce43476a0540f2
antti <antti@laptop.lan>
2014-01-26 19:28:14 +0200

    2.viikko, 10 ekaa

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -13,13 +13,17 @@ class BeersController < ApplicationController
   end
 
   # GET /beers/new
-  def new
-    @beer = Beer.new
-  end
+ def new
+   @beer = Beer.new
+   @breweries = Brewery.all
+   @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
+ end
 
   # GET /beers/1/edit
-  def edit
-  end
+  def edit 
+    @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"] 
+  end 
 
   # POST /beers
   # POST /beers.json
@@ -28,7 +32,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
         format.json { render action: 'show', status: :created, location: @beer }
       else
         format.html { render action: 'new' }
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ActiveRecord::Base
+  def average_rating
+    ratings.average("score")
+  end
+
+  def to_s
+    "#{name} (" + brewery.name + ")"
+  end 
   belongs_to :brewery
+  has_many :ratings
 end
+
