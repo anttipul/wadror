0d81dc1e7db37b8fd9426ecb909f35ac2f0c3271
antti <antti@laptop.lan>
2014-02-09 13:33:03 +0200

    4vko 13-15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,23 @@ class ApplicationController < ActionController::Base
 
   # määritellään, että metodi current_user tulee käyttöön myös näkymissä
   helper_method :current_user
+  helper_method :current_user_is_admin
 
   def current_user
     return nil if session[:user_id].nil? 
     User.find(session[:user_id]) 
+  end
+
+  def current_user_is_admin
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id]).admin?
+  end
+
+  def ensure_that_signed_in 
+    redirect_to signin_path, notice:'you should be signed in' if current_user.nil?
+  end
+  
+  def ensure_that_admin
+    redirect_to signin_path, notice:'you are not admin' if not current_user.admin?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@ b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
 b2.beers.create name:"X Porter", style:"Porter"
 b3.beers.create name:"Hefezeizen", style:"Weizen"
 b3.beers.create name:"Helles", style:"Lager"
+
+User.create username:"admin", password:"Admin1", password_confirmation:"Admin1", admin:"true"
+User.create username:"matti", password:"Matti1", password_confirmation:"Matti1", admin:"false"
